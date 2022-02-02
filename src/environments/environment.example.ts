f2f25d8c91c8f170f24202289ae4f7e968c4ac8c
David Anderson <david.anderson281@yahoo.co.uk>
2022-02-02 13:27:50 +0000

    added guidance around keys

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,6 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The keys below are similar format as expected but are expired.
+// You will need to create new keys for application to work
 
 export const environment = {
   production: false,
@@ -8,12 +7,3 @@ export const environment = {
   RAPIDAPI_KEY: '1457df11e9mshcbd455a747b2b71p10963ajsncd3952ab9e4b',
   RAWG_KEY: '159271bb599c43c88887e31b57059874'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
